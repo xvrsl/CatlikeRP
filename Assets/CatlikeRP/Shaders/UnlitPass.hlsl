7f9ae7bb25ccd85e560c7b382487c6622a23831c
xvrsl <xavierseal@live.com>
2021-01-03 10:07:55 +0800

    SRP Library

--- Assets/CatlikeRP/Shaders/UnlitPass.hlsl
+++ Assets/CatlikeRP/Shaders/UnlitPass.hlsl
@@ -1,8 +1,12 @@
 #ifndef CUSTOM_UNLIT_PASS_INCLUDED
 #define CUSTOM_UNLIT_PASS_INCLUDED
 
-float4 UnlitPassVertex(float3 positionOS):SV_POSITION{
-	return float4(positionOS,1.0);
+#include "../ShaderLibrary/Common.hlsl"
+
+float4 UnlitPassVertex(float3 positionOS:POSITION) :SV_POSITION
+{
+	float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+	return TransformWorldToHClip(positionWS);
 }
 float4 UnlitPassFragment():SV_TARGET{
 	return 0.0; 
